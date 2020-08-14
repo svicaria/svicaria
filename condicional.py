d37cb272ee1b5245d3ae8f46acd573c283670bb0
Santiago Vicaria <vicaria.santiago@gmail.com>
2020-08-14 17:00:03 -0500

    Se agregan ajustes a archivo condicional

--- condicional.py
+++ condicional.py
@@ -1,5 +1,13 @@
-edad = int(input('Escribe tu edad: '))
-if edad > 17:
-    print('Es mayor de edad')
+# edad = int(input('Escribe tu edad: '))
+# if edad > 17:
+#     print('Es mayor de edad')
+# else:
+#     print('Es mayor de edad')
+
+numero = int(input('Escribe un numero: '))
+if numero > 5:
+    print('Es mayor a 5')
+elif numero == 5:
+    print('Es igual a 5')
 else:
-    print('Es menor de edad')
+    print('Es menor a 5')        
