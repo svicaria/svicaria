18597b6520fba62cf9f0e74537f9a3c7927127f5
Santiago Vicaria <vicaria.santiago@gmail.com>
2020-08-15 16:36:05 -0500

    Se agregan nuevos de ejercicios en clase

--- adivina_el_numero.py
+++ adivina_el_numero.py
@@ -3,14 +3,14 @@ import random
 
 def run():
     numero_aleatorio = random.randint(1,100)
-    numero_elegido = int(input('Elige un número del 1 al 100: '))
+    numero_elegido = int(input('Voy a elegir un número al azar del 1 al 100, escribe un número del 1 al 100 para saber si adivinaste: '))
     while numero_elegido != numero_aleatorio:
         if numero_elegido < numero_aleatorio:
             print('El número es más grande')
         else:
             print('El número es mas pequeño')
-            numero_elegido = int(input('Elige otro número: '))
+        numero_elegido = int(input('Elige otro número: '))
     print('¡Ganaste!')
 
 if __name__ == '__main__':
-run()
+    run()
---
+++ diccionarios.py
@@ -0,0 +1,11 @@
+def run():
+    mi_diccionario = {
+        'llave1' : 1,
+        'llave2' : 2,
+        'llave3' : 3,
+    }
+    print(mi_diccionario)
+
+
+if __name__ == "__main__":
+    run()
