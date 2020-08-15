9dd0e694d11c836ec6d957ce7a46c6a028f13d01
Santiago Vicaria <vicaria.santiago@gmail.com>
2020-08-15 17:28:38 -0500

    Se agrega proyecto final, generador de contraseñas

--- diccionarios.py
+++ diccionarios.py
@@ -4,8 +4,27 @@ def run():
         'llave2' : 2,
         'llave3' : 3,
     }
-    print(mi_diccionario)
 
+    # print(mi_diccionario['llave1'])
+    # print(mi_diccionario['llave2'])
+    # print(mi_diccionario['llave3'])
+
+    poblacion_paises = {
+        'Argentina' : 44938712,
+        'Brasil' : 210147125,
+        'Colombia' : 50372424,
+    }
+
+    # print(poblacion_paises['Bolivia'])
+
+    # for pais in poblacion_paises.keys():
+    #     print(pais)
+
+    # for pais in poblacion_paises.values():
+    #     print(pais)
+
+    for pais, poblacion in poblacion_paises.items():
+        print(pais + ' tiene ' + str(poblacion) + ' habitantes')
 
 if __name__ == "__main__":
     run()
