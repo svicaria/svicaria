89d47f21e0c1c80a1aa4912c259834b0856b1f0d
Santiago Vicaria <vicaria.santiago@gmail.com>
2020-08-15 09:55:29 -0500

    Se agrega archivo recorrer y se ajusta con for archivo for

--- for.py
+++ for.py
@@ -4,8 +4,14 @@
 # print(4)
 # print(5)
 
-contador = 1
-print(contador)
-while contador < 1000:
-    contador = contador + 1
-    print(contador)
+# contador = 1
+# print(contador)
+# while contador < 1000:
+#     contador += 1
+#     print(contador)
+
+# for contador in range(1, 1001):
+#     print(contador)
+
+for i in range(10):
+    print(11*i)
