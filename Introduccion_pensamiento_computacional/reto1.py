acbd9e06103eb0f5d55b28e35a6c1c3f62aa0ea1
Santiago Vicaria <vicaria.santiago@gmail.com>
2020-08-16 17:39:58 -0500

    Se reordenan archivos para poder editarlos y correrlos rápidamente, se hace de forma temporal mientras finaliza el curso

--- Introduccion_pensamiento_computacional/reto1.py
+++
@@ -1,9 +0,0 @@
-def main():
-    nombre= str(input('Escribe tu nombre: '))
-    print(f'Hola {nombre} , bienvenido a este programa')
-    lenght = len(nombre)
-    print(f'Tu nombre tiene {lenght} letras')
-    print('Fin del programa')
-
-if __name__ == "__main__":
-    main()
