692d10fe137afca7f77cfd236f806a1ee624a15b
Santiago Vicaria <vicaria.santiago@gmail.com>
2020-08-16 18:00:00 -0500

    Se aggrega algoritmo búsqueda binaria

---
+++ busqueda_binaria.py
@@ -0,0 +1,16 @@
+objetivo = int(input('Escribe un número para calcular su raíz cuadrada: '))
+epsilon = 0.01
+bajo = 0.0
+alto = max(1.0, objetivo)
+respuesta = (alto + bajo) / 2
+
+while abs(respuesta**2 - objetivo) >= epsilon:
+    print(f'bajo={bajo}, alto={alto}, respuesta={respuesta}')
+    if respuesta**2 < objetivo:
+        bajo = respuesta
+    else:
+        alto = respuesta
+
+    respuesta = (alto + bajo) / 2
+
+print(f'La raíz cuadrada de {objetivo} es {respuesta}')
