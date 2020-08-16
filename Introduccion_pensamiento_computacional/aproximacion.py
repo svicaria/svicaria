f2a039f35fe01c215acb5d95152d67fa3bc80b73
Santiago Vicaria <vicaria.santiago@gmail.com>
2020-08-16 17:38:32 -0500

    Se agregan archivos

--- Introduccion_pensamiento_computacional/aproximacion.py
+++ Introduccion_pensamiento_computacional/aproximacion.py
@@ -1,1 +1,13 @@
-aproximacion.py
+objetivo = int(input('Escoge un número: '))
+epsilon = 0.01
+paso = epsilon**2
+respuesta = 0.0
+
+while abs(respuesta**2 - objetivo) >= epsilon and respuesta <= objetivo :
+    print(abs(respuesta**2 - objetivo), respuesta)
+    respuesta += paso
+
+if abs(respuesta**2 - objetivo) >= epsilon:
+    print(f'No se encontró la raíz cuadrada de {objetivo}')
+else:
+    print(f'La raíz cuadrada de {objetivo} es {respuesta}')
