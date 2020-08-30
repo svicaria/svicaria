20178498db29fad4a101700ae92b02c0c6213497
Santiago Vicaria <vicaria.santiago@gmail.com>
2020-08-30 18:07:44 -0500

    Se agrega archivo terminado herencia y archivo polimonrfismo en construcción

--- herencia.py
+++ herencia.py
@@ -1,1 +1,22 @@
-herencia.py
+
+class Rectangulo:
+
+    def __init__(self, base, altura):
+        self.base = base
+        self.altura = altura
+
+    def area(self):
+        return self.base * self.altura
+
+class Cuadrado(Rectangulo):
+
+        def __init__(self, lado):
+            super().__init__(lado, lado)
+
+
+if __name__ == "__main__":
+    Rectangulo = Rectangulo(base=3, altura=4)
+    print(Rectangulo.area())
+
+    Cuadrado = Cuadrado(lado=5)
+    print(Cuadrado.area())
